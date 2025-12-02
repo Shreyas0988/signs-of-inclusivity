2566431d2e63d326670da939c307d35bb9e67096
Shreyas0988 <145433465+Shreyas0988@users.noreply.github.com>
2025-12-03 01:03:57 +0800

    Update socketio.run to use environment port

--- app.py
+++ app.py
@@ -379,4 +379,7 @@ def handle_stream_frames(data):
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=8000, debug=True, allow_unsafe_werkzeug=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    socketio.run(app, host="0.0.0.0", port=port)
+
